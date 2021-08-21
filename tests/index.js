16dc54cc310e654a1c974cd27c29a462e69c3445
Luis Adame <luis@luisadame.ninja>
2021-08-21 13:51:36 +0200

    test: add test case for more columns

--- tests/index.js
+++ tests/index.js
@@ -112,4 +112,35 @@ test("it sorts bucket of objects", () => {
   assert.equal(sorted, expected);
 });
 
+test("it sorts bucket of objects with different columns", () => {
+  const items = [
+    { id: "1", ratio: 3 },
+    { id: "2", ratio: 1 },
+    { id: "3", ratio: 2 },
+    { id: "4", ratio: 2 },
+    { id: "5", ratio: 1 },
+    { id: "7", ratio: 1 },
+    { id: "8", ratio: 3 },
+  ];
+  const accessor = (item) => item.ratio;
+  const itemsByValue = groupItemsByValue(items, accessor);
+  const sorted = makeGrid(itemsByValue, 5, accessor);
+  const expected = [
+    [
+      { id: "1", ratio: 3 },
+      { id: "3", ratio: 2 },
+    ],
+    [
+      { id: "4", ratio: 2 },
+      { id: "8", ratio: 3 },
+    ],
+    [
+      { id: "2", ratio: 1 },
+      { id: "5", ratio: 1 },
+      { id: "7", ratio: 1 },
+    ],
+  ];
+  assert.equal(sorted, expected);
+});
+
 test.run();
